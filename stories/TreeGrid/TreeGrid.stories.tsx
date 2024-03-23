48743965980ac045719c4aa0c00ec3957cd5b49e
Vladimir Kubantsev <trihlord@yandex.ru>
2024-03-23 16:25:28 +0300

    feat: tree grid country story

--- stories/TreeGrid/TreeGrid.stories.tsx
+++ stories/TreeGrid/TreeGrid.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { TreeGrid } from "./TreeGrid";
+import { TreeGrid, type TreeGridColumn, type TreeGridRow } from "./TreeGrid";
 
 const meta: Meta<typeof TreeGrid> = {
   component: TreeGrid,
@@ -10,3 +10,13 @@ export default meta;
 type Story = StoryObj<typeof TreeGrid>;
 
 export const Basic: Story = {};
+
+export const Countries: Story = {
+  render: function () {
+    type Country = { id: number; name: string; postcode: number };
+    const countries: Country[] = [{ id: 0, name: "Russia", postcode: 117 }];
+    const columns: TreeGridColumn<Country>[] = [{ key: "name" }];
+    const row: TreeGridRow<Country> = { key: "id" };
+    return <TreeGrid columns={columns} data={countries} row={row} />;
+  },
+};
--- stories/TreeGrid/TreeGrid.tsx
+++ stories/TreeGrid/TreeGrid.tsx
@@ -1,3 +1,86 @@
-export function TreeGrid() {
-  return <table></table>;
+import type { CSSProperties, Key, ReactNode } from "react";
+
+export type TreeGridRender<T> =
+  | ((data: T) => ReactNode)
+  | { [x in keyof T]: T[x] extends ReactNode ? x : never }[keyof T];
+
+export interface TreeGridDataProps<T> {
+  data: T;
+  render?: TreeGridRender<T>;
+}
+
+export function TreeGridData<T extends object>({
+  data,
+  render,
+}: TreeGridDataProps<T>) {
+  if (!render) {
+    return null;
+  }
+  if (typeof render === "function") {
+    return render(data);
+  }
+  if (render in data) {
+    return data[render];
+  }
+  return null;
+}
+
+export interface TreeGridColumn<T> {
+  key: Key;
+  title?: ReactNode;
+  width?: CSSProperties["width"];
+  render?: TreeGridRender<T>;
+}
+
+export interface TreeGridRow<T> {
+  key: { [x in keyof T]: T[x] extends Key ? x : never }[keyof T];
+}
+
+export interface TreeGridProps<T> {
+  columns?: TreeGridColumn<T>[];
+  data?: T[];
+  row: TreeGridRow<T>;
+}
+
+export function TreeGrid<T extends object>({
+  columns = [],
+  data = [],
+  row,
+}: TreeGridProps<T>) {
+  return (
+    <table role="treegrid">
+      <colgroup>
+        {columns.map(function ({ key, width }) {
+          return <col key={key} style={{ width }} />;
+        })}
+      </colgroup>
+      <thead>
+        <tr>
+          {columns.map(function ({ key, title }) {
+            return (
+              <th key={key} scope="col">
+                {title}
+              </th>
+            );
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(function (data) {
+          const key = data[row.key] as Key;
+          return (
+            <tr key={key}>
+              {columns.map(function ({ key, render }) {
+                return (
+                  <td key={key} role="gridcell">
+                    <TreeGridData data={data} render={render} />
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
